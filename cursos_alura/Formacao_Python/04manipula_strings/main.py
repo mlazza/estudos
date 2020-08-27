fed5b1348250c9e888a7f47652f88011a64d06b8
Marlon-Nav <mlazzarotti@gmail.com>
2020-08-27 11:03:51 -0300

    updating curso alura-python

--- cursos_alura/Formacao_Python/04manipula_strings/main.py
+++ cursos_alura/Formacao_Python/04manipula_strings/main.py
@@ -1,4 +1,4 @@
-""""
+"""
 sobreMim = "Meu nome é Rodrigo e minha idade é 26"
 #meuNome = "Rodrigo"
 #............0123456
@@ -13,7 +13,7 @@ argumento = "moedaorigem=real"
 subString = argumento[5:11]
 print(subString)
 '''
-
+'''
 argumento = "moedaorigem=real"
 
 lista_args = argumento.split("=")
@@ -22,3 +22,27 @@ print(lista_args)
 index = argumento.find("=")
 subString = argumento[index + 1:]
 print(subString)
+'''
+"""
+#None é diferente de vazio ''
+
+string = ''
+
+if string:
+	print('Tem algo aqui')
+else:
+	print('Nao tem algo aqui')
+"""
+
+from ExtratorArgumentosUrl import ExtratorArgumentosUrl
+
+url = "moedaorigem=real&moedadestino=dolar"
+#url = None
+#url = ''
+argumentosUrl = ExtratorArgumentosUrl(url)
+#print(argumento)
+#print(ExtratorArgumentosUrl.urlEhValida(url))
+
+moedaOrigem, moedaDestino = argumentosUrl.extraiArgumentos()
+
+print(moedaDestino, moedaOrigem)
