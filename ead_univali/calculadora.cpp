1fe9ac1627a46036d436e5bac4a969f0e01a388e
Marlon Ubuntu <mlazzarotti@gmail.com>
2021-03-16 14:49:55 -0300

    upd calc

--- ead_univali/calculadora.cpp
+++ ead_univali/calculadora.cpp
@@ -29,6 +29,7 @@ int main()
         cout << "\nESCOLHA A OPÇÃO DESEJADA:";
         cout << "\n[1] - converte decimal em binario";
         cout << "\n[2] - converte binario em decimal";
+        cout << "\n[0] - SAIR";
 
         cin >> opcao;
 
